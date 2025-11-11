6539dc4b39ae063b5c916a4c00e09a3b3b9df5ce
Asib Iqbal <160288863+iqbalasib@users.noreply.github.com>
2025-11-11 11:27:29 +0600

    Add files via upload

---
+++ task_7lab2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string first, last;
+    cout << "Enter first name: ";
+    cin >> first;
+    cout << "Enter last name: ";
+    cin >> last;
+
+    cout << "Full name = " << first << " " << last << endl;
+    return 0;
+}
